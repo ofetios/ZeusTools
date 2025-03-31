97ae54631ad13e04c8379d8c1979a5865f4c4130
ofetios <ofetios@proton.me>
2025-03-31 22:58:27 +0300

    refactor: removed .hemttout directory from git history

--- .hemttout/dev/addons/sequencer/config.cpp
+++
@@ -1,8 +0,0 @@
-#include "modules\HelloWorld\config.cpp"
-
-class CfgFactionClasses {
-    class NO_CATEGORY;
-    class MyModuleCategory : NO_CATEGORY {
-        displayName = "My Custom Modules";
-    };
-};
