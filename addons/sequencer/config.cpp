755be117003c8729f4685c7603afb4715c19f89e
ofetios <ofetios@proton.me>
2025-04-01 21:43:20 +0300

    refactor: prevent multiple class declararations

--- addons/sequencer/config.cpp
+++ addons/sequencer/config.cpp
@@ -1,8 +1,22 @@
-#include "modules\HelloWorld\config.cpp"
+class CfgPatches {
+    class ZT_Sequencer {
+        units[] = {"ZT_Module_HelloWorld"};
+    };
+};
 
 class CfgFactionClasses {
     class NO_CATEGORY;
     class MyModuleCategory : NO_CATEGORY {
         displayName = "My Custom Modules";
+    };
+};
+
+class CfgVehicles {
+	#include "modules\HelloWorld\CfgVehicles.hpp"
+};
+
+class CfgFunctions {
+    class ZT_Sequencer {
+        #include "modules\HelloWorld\CfgFunctions.hpp"
     };
 };
