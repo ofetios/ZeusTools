7766298740e8f04b4b3bf562cf9c0a293c090bb6
royrovp <159064839+royrovp@users.noreply.github.com>
2025-04-12 17:00:25 +0200

    feat: Added ALL/ANY selector to AwaitTriggers (#36)

--- addons/sequencer/modules/AwaitTriggers/CfgVehicles.hpp
+++ addons/sequencer/modules/AwaitTriggers/CfgVehicles.hpp
@@ -10,6 +10,26 @@ class ZT_Module_AwaitTriggers  : BaseSequenceModule {
 	curatorCanAttach = 1;
 	functionContext = "spawn";
 
+	class Attributes : AttributesBase {
+        class allAny: Combo {
+            property = "ZT_AwaitTriggers_AllAny";
+            displayName = "All / Any Triggers";
+            description = "How many triggers need to be activated for the sequence to continue";
+            typeName = "NUMBER";
+            defaultValue = 0;
+            class values {
+                class Option1 {
+                    name = "ALL";
+                    value = 0;
+                };
+                class Option2 {
+                    name = "ANY";
+                    value = 1;
+                };
+            };
+        };
+	};
+
 	class ModuleDescription : ModuleDescription {
 		description[] = {
 			"Waits for all synced triggers to be activated before continuing"
